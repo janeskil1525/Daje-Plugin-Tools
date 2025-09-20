fb50e14ce8672717feafd94426a8a91bf25ae52c
janeskil1525 <janeskil1525@gmail.com>
2025-09-20 16:32:46 +0200

    Even More Treelist

--- lib/Daje/Database/Helper/TreeList.pm
+++ lib/Daje/Database/Helper/TreeList.pm
@@ -2,6 +2,8 @@ package Daje::Database::Helper::TreeList;
 use Mojo::Base  -base, -signatures, -async_await;
 use v5.40;
 
+use Data::Dumper;
+
 use Daje::Database::Model::ToolsProjects;
 use Daje::Database::Model::ToolsVersion;
 
@@ -9,16 +11,18 @@ has 'db';
 
 async sub load_treelist($self, $tools_projects_pkey) {
 
-    my $treelist = {};
+    my $treelist;
     # my $project = $self->_load_project($tools_projects_pkey);
     # $self->_add_node($treelist, $project->{data}, 'tools_projects');
 
     my $versions = $self->_load_versions($tools_projects_pkey);
+    say "Daje::Database::Helper::TreeList::load_treelist" . Dumper($versions);
     my $length = scalar @{$versions->{data}};
     for (my $i = 0; $i < $length; $i++) {
 
 
-        $self->_add_node($treelist, @{$versions->{data}}[$i], 'tools_version', 0);
+        my $node = $self->_add_node($treelist, @{$versions->{data}}[$i], 'tools_version', 0);
+        push (@{$treelist->{data}}, $node);
 
     }
 
@@ -28,14 +32,16 @@ async sub load_treelist($self, $tools_projects_pkey) {
 
 sub _add_node($self, $treelist, $data, $type, $level) {
 
-    my $res->{id} = $data->{tools_versions_pkey} . "-" . $type;
+    say "Daje::Database::Helper::TreeList::_add_node data " . Dumper($data);
+    my $res->{id} = $data->{tools_version_pkey} . "-" . $type;
     $res->{label} = $data->{name} ;
-    $res->{data} = $data->{name} . ' ' . $data->{state} ;
+    $res->{data} = $data->{name} . ' ' . $data->{version} ;
     $res->{icon} = 'pi pi-fw pi-inbox';
     $res->{children} = [];
-
+    say "Daje::Database::Helper::TreeList::_add_node res " . Dumper($res);
 
     my $test = 1;
+    return $res;
 }
 
 sub _load_versions($self, $tools_projects_pkey) {
--- lib/Daje/Database/Model/Super/ToolsVersion.pm
+++ lib/Daje/Database/Model/Super/ToolsVersion.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Daje::Database::Model::Super::Common::Base', -base, -signatures,
 
 use Data::Dumper;
 
-has 'fields' => "tools_version_pkey, editnum, insby, insdatetime, modby, moddatetime, tools_projects_fkey, version, locked";
+has 'fields' => "tools_version_pkey, editnum, insby, insdatetime, modby, moddatetime, tools_projects_fkey, version, locked, name";
 has 'primary_key_name' => "tools_version_pkey";
 has 'table_name' => "tools_version";
 
